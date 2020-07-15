3bf940b86ceda9cfc78b011b05e1e0c5022403bc
jonkb <jonathan.knight.black@gmail.com>
2020-07-15 12:55:13 -0500

    Specify the correct error for singularities. Other tweaks for testing math_util.py

--- src/math_util.py
+++ src/math_util.py
@@ -1,8 +1,7 @@
 import math
 import numpy as np
 import sympy as sym
-from sympy.calculus.singularities import singularities
-from sympy.sets import Interval
+#from sympy.sets import Interval
 
 eps = 1e-14
 default_sigfig = 4
@@ -57,8 +56,9 @@ def pw_sdls(f, x):
 #Supports everything that singularities supports as well as Piecewise
 def discontinuities(f, x, dom=None):
 	discont = np.array([])
-	try: sings = singularities(f, x, domain=Interval.open(*dom))
-	except: pass #singularities throws exceptions for Piecewise
+	#Try the pre-made singularities function to see if it's enough
+	try: sings = sym.singularities(f, x)#, domain=Interval.open(*dom))
+	except NotImplementedError: pass #singularities throws exceptions for Piecewise
 	else: discont = np.array(list(sings)).astype(np.float64)
 	
 	pw_sd_limits = pw_sdls(f,x)
@@ -112,7 +112,7 @@ def max1d(f, x, dom):
 def max2d(f, x, y, xdom, ydom):
 	x0, x1 = xdom
 	y0, y1 = ydom
-	print(61, xdom, ydom)
+	#print(61, xdom, ydom)
 	f_lam = sym.lambdify((x,y), f, "numpy")
 	#print("f: ", f)
 	#print("dfdx: ", f.diff(x))
@@ -121,7 +121,7 @@ def max2d(f, x, y, xdom, ydom):
 	#print(63, grad0 )
 	#print(64, sym.solve(grad0, (x,y)) )
 	critpts = np.array(sym.solve(grad0, (x,y))).astype(np.float64)
-	print(69, critpts)
+	#print(69, critpts)
 	cpts_in_dom = np.empty((0,2), float)
 	for pt in critpts:
 		if pt[0] > xdom[0] and pt[0] < xdom[1] and pt[1] > ydom[0] and pt[1] < ydom[1]:
@@ -129,7 +129,7 @@ def max2d(f, x, y, xdom, ydom):
 	critpts = cpts_in_dom
 	#critpts = np.array( [pt for pt in critpts if 
 	#	(pt[0] > xdom[0] and pt[0] < xdom[1] and pt[1] > ydom[0] and pt[1] < ydom[1])] )
-	print(77, critpts)
+	#print(77, critpts)
 	critvals = []
 	if len(critpts) > 0:
 		critvals = f_lam(critpts[:,0], critpts[:,1])
@@ -138,26 +138,25 @@ def max2d(f, x, y, xdom, ydom):
 	N_bound = f.subs(y, y1)
 	(N_max, N_max_x), (N_min, N_min_x) = max1d(N_bound, x, (x0, x1))
 	critvals = np.append(critvals, [N_max, N_min])
-	print(92, N_bound)
-	print(93, critpts)
+	#print(92, N_bound)
+	#print(93, critpts)
 	critpts = np.append(critpts, [[N_max_x, y1], [N_min_x, y1]], axis=0)
-	print(94, critpts)
+	#print(94, critpts)
 	E_bound = f.subs(x, x1)
 	(E_max, E_max_y), (E_min, E_min_y) = max1d(E_bound, y, (y0, y1))
 	critvals = np.append(critvals, [E_max, E_min])
 	critpts = np.append(critpts, [[x1, E_max_y], [x1, E_min_y]], axis=0)
-	print(99, critpts)
+	#print(99, critpts)
 	S_bound = f.subs(y, y0)
 	(S_max, S_max_x), (S_min, S_min_x) = max1d(S_bound, x, (x0, x1))
 	critvals = np.append(critvals, [S_max, S_min])
 	critpts = np.append(critpts, [[S_max_x, y0], [S_min_x, y0]], axis=0)
-	print(104, critpts)
+	#print(104, critpts)
 	W_bound = f.subs(x, x0)
 	(W_max, W_max_y), (W_min, W_min_y) = max1d(W_bound, y, (y0, y1))
 	critvals = np.append(critvals, [W_max, W_min])
 	critpts = np.append(critpts, [[x0, W_max_y], [x0, W_min_y]], axis=0)
 	print(109, critpts)
-	#print(56, critpts)
 	f_max = np.max(critvals)
 	#print(89, np.where(critvals == f_max))
 	xy_max = critpts[np.where(critvals == f_max)[0][0]]
@@ -168,12 +167,22 @@ def max2d(f, x, y, xdom, ydom):
 
 
 
-def test():
+def test_maxmin():
 	x = sym.symbols('x')
 	f = sym.sin(x)
 	print("max1d('sin(x)', x, [0, pi+.001])")
 	print(max1d(f, x, [0, math.pi+.001]))
+	
+	f1 = 1/x
+	print("max1d('1/x', x, [-1, 1])")
+	print(max1d(f1, x, [-1, 1]))
+	
+	f2 = sym.Piecewise((x, x<0), (2-.1*x, True))
+	print("max1d('{(x, x<0), (2-.1*x, True)', x, [-1, 1])")
+	print(max1d(f2, x, [-1, 1]))
+	
 	y = sym.symbols('y')
 	fxy = sym.sin(x)+1.1*sym.sin(y)
+	print("max2d(sin(x)+1.1*sin(y), x, y, [0, math.pi+.001], [0, math.pi+.001])")
 	print(max2d(fxy, x, y, [0, math.pi+.001], [0, math.pi+.001]))
-#test()
+#test_maxmin()
